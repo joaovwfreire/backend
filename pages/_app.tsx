dd9f2102f9d287a28c7eb1598f884d0f5fd3ce16
Kerim <kerimberdi.agayev@edusoftech.eu>
2022-12-22 15:55:46 +0200

    footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 // Thirweb
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import { SessionProvider } from 'next-auth/react';
+import Footer from './components/Footer';
 
 // Polygon Mumbai
 const activeChainId = ChainId.Mumbai;
@@ -13,7 +14,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThirdwebProvider desiredChainId={activeChainId}>
       <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <footer>
+        <Footer/>
+      </footer>
       </SessionProvider>
+      
       
     </ThirdwebProvider>
   )
---
+++ pages/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex w-full bg-gray-900 text-gray-300 items-center justify-center p-4 flex-col space-y-4'>
+        <hr className="h-px bg-gray-400 border-0 w-full"/>
+        <p>&copy; Game Payy, 2022</p>
+
+    </div>
+  )
+}
+
+export default Footer
