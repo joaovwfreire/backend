45e2313a128a0871c68d2ea5db27f3519b516afa
Kerim <kerimberdi.agayev@edusoftech.eu>
2022-12-27 12:38:26 +0200

    db setup fix

--- server/mongo/dbSetup.ts
+++ server/mongo/dbSetup.ts
@@ -2,8 +2,6 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = `mongodb+srv://jovi:${process.env.MONGO_DB_PASSWORD}@cluster0.n32qot2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
