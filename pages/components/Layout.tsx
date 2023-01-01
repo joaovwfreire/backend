2b506356dab590787764c52ee52cac2528f71809
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-01-01 23:33:28 +0200

    layout

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,19 +1,7 @@
-import { Session } from "next-auth";
 import React from "react";
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 
-interface Props {
-  session: Session;
-}
-
-const Layout = ({ session }: Props) => {
-  return (
-    <div>
-      <Sidebar />
-      <Navbar session={session} />
-    </div>
-  );
+const Layout = () => {
+  return <div>Layout</div>;
 };
 
 export default Layout;
