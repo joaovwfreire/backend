0adc16eac7ab04f64cd481a2dbdd60572504286b
jovi <77895278+joaovwfreire@users.noreply.github.com>
2023-01-09 16:48:45 -0300

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,16 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import env from "@beam-australia/react-env";
+
+
 
 
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: env("GOOGLE_CLIENT_ID") as string,
+      clientSecret: env("GOOGLE_CLIENT_SECRET") as string,
     })
   ],
   pages: {
