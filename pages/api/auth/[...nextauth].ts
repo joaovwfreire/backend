c875dc717b0f2e8bbe0e31b0bdbf461af5d7f3a0
joaovwfreire <joao.freire@fm.usp.br>
2023-01-15 13:52:44 -0300

    discord oauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,19 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import env from "@beam-australia/react-env";
+import DiscordProvider from "next-auth/providers/discord";
 
 
 
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers
   providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
   ],
   pages: {
