2486bdc206f12ec1c15f5aec33f21b83a8ebb96c
joaovwfreire <joao.freire@fm.usp.br>
2023-01-17 16:35:27 -0300

    cs go api

--- pages/api/steam/csgo/player.ts
+++ pages/api/steam/csgo/player.ts
@@ -1,22 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "../../../server/mongo/middleware/mongodb";
-import { unstable_getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]";
+import clientPromise from "../../../../server/mongo/middleware/mongodb";
+import axios from "axios";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const session = await unstable_getServerSession(req, res, authOptions);
-
-  if (!session) {
-    res.status(400).json({ Error: "No session" });
-  } else {
+  
     const email = req.body.email;
     const steamId = req.body.steam_id;
 
-    if (req.method === "POST" && email && steamId) {
+    if (req.method === "GET") {
       try {
+        let response = await axios.get(
+            `https://public-api.tracker.gg/v2/csgo/standard/profile/steam/76561198008049283`,
+            {
+              headers: {
+                'TRN-Api-Key': process.env.TRN_API_KEY as string
+              },
+            }
+          );
+console.log(response.data)
+          res.json(response.data)
         const client = await clientPromise;
         const db = client.db("gamepayy");
 
@@ -32,5 +37,5 @@ export default async function handler(
         throw new Error(e as string).message;
       }
     }
-  }
+  
 }
